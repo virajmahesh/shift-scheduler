653299fbbb42e9939891ee61d1df45bf29a59202
Gaurav Garg <garggaurav@berkeley.edu>
2016-03-09 05:32:10 +0000

    Enforce shift limits
    Prevent users from signing up for shifts that are filled up. Users can see which shifts are filled and how many spots are available for the shift they are signing up for.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,57 @@ class ApplicationController < ActionController::Base
   def index
     @user = User.find_by id: session[:user_id]
   end
+  
+  def shift
+    @shift = Shift.find_by id: params[:id]
+    @user = User.find_by id: session[:user_id]
+    @num_volunteers = VolunteerCommitment.where(shift_id: params[:id]).size
+    if @shift.nil?
+      flash[:error] = "Shift not found."
+      redirect_to "/"
+    end  
+  end
+  
+  def shift_signUp
+    @shift = Shift.find_by id: params[:id]
+    @user = User.find_by id: session[:user_id]
+    @num_volunteers = VolunteerCommitment.where(shift_id: params[:id]).size
+    
+    if @shift.has_limit and @shift.limit <= @num_volunteers
+      flash[:error] = "Shift already full"
+      redirect_to "/shift/"+params[:id] and return
+    end
+    
+    if @user.nil?
+      flash[:error] = "Login Required"
+      redirect_to "/" and return
+    end
+    
+    if @num_volunteers < @shift.limit and not @user.nil?
+      VolunteerCommitment.create({:user_id => @user.id, :shift_id => @shift.id, :created_at => Time.now, :updated_at => Time.now})
+      flash[:notice] = "You have been signed up for the shift"
+      redirect_to "/shift/"+params[:id] and return
+    end
+  end
+  
+  def event
+    @event = Event.find_by id: params[:id]
+
+    if @event.nil?
+      flash[:error] = "Event not found"
+      redirect_to "/"
+    end
+    
+    @shifts = Shift.where event_id: params[:id]
+    @numFreeShifts = 0
+    @num_volunteers = {}
+    
+    @shifts.each do |shift|
+    @num_volunteers[shift.id] = VolunteerCommitment.where(shift_id: shift.id).size
+    
+    if shift.has_limit and @num_volunteers[shift.id] < shift.limit
+      @numFreeShifts = @numFreeShifts + 1
+    end
+  end
+end
 end
--- features/shift_limit_user.feature
+++ features/shift_limit_user.feature
@@ -5,25 +5,21 @@ Feature: Event Creators Can limit the number of users that can sign up for a shi
   so that I don’t have too many volunteers for the same shift
 
   Scenario: attempt to sign up for shift that has space
-    Given shift 1 has a limit
-    And the limit of shift 1 is 5
+    Given shift 1 has a limit 5
     And shift 1 has 4 users
-    Given the user John Doe exists
+    Given user John Doe exists with password hellboy
+    And user John Doe is logged in with password hellboy
     When John Doe signs up for shift 1
-    Then shift 1 should have one more user
-    And John Doe should have one more commitment
-    And John Doe should be on the event page
-    And John Doe should see 'You have been signed up for the shift' on the event page
+    Then shift 1 should have 5 users
+    And John Doe should have 1 commitments
+    And John Doe should see "You have been signed up"
 
   Scenario: attempt to sign up for shift that is at capacity
-    Given shift 1 has a limit
-    And the limit of shift 1 is 5
+    Given shift 1 has a limit 5
     And shift 1 has 5 users
-    Given the user John Doe exists
-    When John Doe sign up for shift 1
-    Then John Doe's volunteer commitments should not change
-    And John Doe should be on the event page
-    And John Doe should see 'Shift already full' on the event page
-
-
-
+    Given user John Doe exists with password hellboy
+    And user John Doe is logged in with password hellboy
+    When John Doe signs up for shift 1
+    Then shift 1 should have 5 users
+    And John Doe should have 0 commitments
+    And John Doe should see "Shift already full"
