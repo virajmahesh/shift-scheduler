41572bfdce51e20077e5ef8fb51f7ce9ed0b5728
Kalon Cheung <kalon@berkeley.edu>
2016-03-11 05:28:42 +0000

    fixed camelCase variables

--- features/step_definitions/custom_steps.rb
+++ features/step_definitions/custom_steps.rb
@@ -46,8 +46,8 @@ Given(/^user (.*) exists with password (.*)$/) do |username, password|
               password: password, password_confirmation: password
 end
 
-Given(/^shift (\d+) has (\d+) users$/) do |shift_id, numUsers|
-  (1..numUsers.to_i).each do |index|
+Given(/^shift (\d+) has (\d+) users$/) do |shift_id, num_users|
+  (1..num_users.to_i).each do |index|
     VolunteerCommitment.create(:user_id => 2, :shift_id => shift_id)
   end
 end
@@ -64,27 +64,27 @@ When(/^(.*) signs up for shift (\d+)$/) do |username, shift_id|
   click_link("Join")
 end
 
-Then(/^shift (\d+) should have (\d+) users$/) do |shift_id, numUsers|
+Then(/^shift (\d+) should have (\d+) users$/) do |shift_id, num_users|
   num_volunteers = VolunteerCommitment.where(shift_id: shift_id).size
-  expect(num_volunteers).to eq(numUsers.to_i)
+  expect(num_volunteers).to eq(num_users.to_i)
 end
 
-Then(/^(.*) should have (\d+) commitments$/) do |username, numCommitments|
+Then(/^(.*) should have (\d+) commitments$/) do |username, expected_commitments|
   user = User.find_by(:username => username)
   num_commitments = VolunteerCommitment.where(user_id: user.id).size
-  expect(num_commitments).to eq(numCommitments.to_i)
+  expect(num_commitments).to eq(expected_commitments.to_i)
 end
 
-Given(/^event (\d+) exists$/) do |eventID|
-  Event.create(:id => eventID, :user_id => 1, :description => "Go Bernie!", :location => "Berkeley", :candidate => "Bernie Sanders")
+Given(/^event (\d+) exists$/) do |event_id|
+  Event.create(:id => event_id, :user_id => 1, :description => "Go Bernie!", :location => "Berkeley", :candidate => "Bernie Sanders")
 end
 
-Given(/^event (\d+) has (\d+) shifts with (\d+) spaces available$/) do |eventID, numShifts, limit|
-  (1..numShifts.to_i).each do
-    Shift.create(:start_time => Time.now, :end_time => Time.now, :event_id => eventID, :limit => limit.to_i, :has_limit => true, :role => 'Tabler')
+Given(/^event (\d+) has (\d+) shifts with (\d+) spaces available$/) do |event_id, num_shifts, limit|
+  (1..num_shifts.to_i).each do
+    Shift.create(:start_time => Time.now, :end_time => Time.now, :event_id => event_id, :limit => limit.to_i, :has_limit => true, :role => 'Tabler')
   end
 end
 
-Given(/^* visit the event (\d+) page$/) do |eventID|
-  visit event_path(eventID)
+Given(/^* visit the event (\d+) page$/) do |event_id|
+  visit event_path(event_id)
 end
