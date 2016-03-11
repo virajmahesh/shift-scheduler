1b72951a2d78b00eefa6ef1db8a812774b40314e
Sidharth Goel <sidharthgoel@berkeley.edu>
2016-03-11 09:45:21 -0800

    Send email notifications when users join and leave shifts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,22 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :volunteer_commitments
 
+  acts_as_messageable
+
   validates :username, presence: true
   validates_uniqueness_of :username, message: 'An account with that username already exists'
   validates_uniqueness_of :email, message: 'An account with that email address already exists'
 
   def signed_up_for shift
     return VolunteerCommitment.exists? user: self, shift: shift
+  end
+
+  def name
+    self.username
+  end
+
+  def mailboxer_email object
+    self.email
+
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+    factory :user do
+        email "aabb@hh.de"
+        password "ruby"
+        password_confrimation "ruby"
+    end
+end
