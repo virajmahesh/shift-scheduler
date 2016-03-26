b2803336af97cec1155978f42ddc29dde46dc811
Sidharth Goel <sidharthgoel@berkeley.edu>
2016-03-26 16:20:52 -0500

    Display number of users signed up for an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class Event < ActiveRecord::Base
   belongs_to :user
   has_many :shifts
@@ -16,4 +18,17 @@ class Event < ActiveRecord::Base
   def formatted_event_date
     self.event_date.strftime '%B %-d %Y'
   end
+
+  def number_signed_up
+    users_signed_up = Set.new
+    self.shifts.each do |s|
+        users_signed_up.add(s.users.ids)
+    end
+    if users_signed_up.length > 0
+      users_signed_up.length.to_s + " user(s) signed up."
+    else
+      "Be the first to sign up for the event."
+    end
+  end
+
 end
