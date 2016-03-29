ef534e250521c09ac49a6699fd338ed81e455cf7
Viraj Mahesh <virajmahesh@gmail.com>
2016-03-29 11:25:51 -0700

    Seed the DB with information for testing
    Create sample users, events and shifts so that this information is available at deploy time

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create email: 'test_user@gmail.com', username: 'test_user',
+                   password: 'test_password', password_confirmation: 'test_password',
+                   city: 'Berkeley', state: 'CA', zip_code: '94704', phone_number: '510-646-5945'
+
+hillary_event = Event.create user: user, event_name: 'Caucus for Hillary',
+                             location: 'Berkeley, CA', event_date: '10/04/2018',
+                             candidate: 'Hillary Clinton', description: 'Come out and support Hillary by caucusing for her'
+
+bernie_event = Event.create user: user, event_name: 'Bake sale for Bernie',
+                            location: 'Berkeley, CA', event_date: '09/07/2018',
+                            candidate: 'Bernie Sanders', description: 'Come out and support Bernie at this bakesale'
+
+hillary_shift = Shift.create event: hillary_event, start_time: '10:00 AM', end_time: '10:40 AM',
+                             role: 'Setup', has_limit: 'true', limit: '10'
+
+bernie_shift = Shift.create event: bernie_event, start_time: '11:20 AM', end_time: '10:00 PM',
+                             role: 'Baking', has_limit: 'false'
