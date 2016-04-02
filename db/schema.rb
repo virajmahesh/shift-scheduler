0472e4116dd27f3854e9683023eefe1dfb1f28f2
Viraj Mahesh <virajmahesh@gmail.com>
2016-04-02 09:53:05 -0700

    Fix broken links in shift views
    - Fix broken "Back to Event" link in shift edit.html.erb
    - Fix broken "Add Shift" link in shift show.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331072202) do
+ActiveRecord::Schema.define(version: 20160401010655) do
 
   create_table "activity_types", force: :cascade do |t|
     t.string   "activity"
