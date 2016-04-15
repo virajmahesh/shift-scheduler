3f5b62eba88062c2c650972161340b102cc2cbe6
Sidharth Goel <sidharthgoel@berkeley.edu>
2016-04-15 11:28:29 -0700

    Angular for create/edit Event

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,12 @@
 //= require_tree .
 
 app = angular.module('icsi-shift-scheduler',['ngMaterial']);
+
+app.controller('EventsController', function($scope){
+  $scope.myDate = new Date();
+  $scope.minDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() - 2,
+      $scope.myDate.getDate());
+
+});
