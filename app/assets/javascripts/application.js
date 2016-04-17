e2816c5a69c7de807fa00beafa895e0dd4e16d3e
Viraj Mahesh <virajmahesh@gmail.com>
2016-04-16 20:45:02 -0700

    Add skills during user registration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,39 @@ app.config(function ($mdDateLocaleProvider) {
     };
 });
 
-app.controller('EventsController', function ($scope) {
+app.controller('EventController', function ($scope) {
     $scope.init = function () {
         $('.md-datepicker-input').attr('name', 'event[event_date]');
     }
+});
+
+function skillID(skill) {
+    return skill.id;
+}
+
+app.controller('RegistrationController', function($scope) {
+    $scope.skills = [];
+
+    // Populate the list of all skills from the API
+    $.get('/skills', function(data) {
+        $scope.allSkills = data;
+    });
+
+    $scope.findSkills = function(query) {
+        var lowercaseQuery = angular.lowercase(query);
+
+        function filterSkillsByQuery(s) {
+            return (angular.lowercase(s.description).indexOf(lowercaseQuery) != -1);
+        }
+
+        function filterByUnused(s) {
+            return $scope.skills.map(skillID).indexOf(s.id) == -1;
+        }
+
+        return $scope.allSkills.filter(filterByUnused).filter(filterSkillsByQuery);
+    };
+
+    $scope.$watch('skills.length', function() {
+        $('#skills').val($scope.skills.map(skillID));
+    });
 });
