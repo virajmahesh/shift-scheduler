dc460a0cc87e8842ff2fa36770d702dd82ac0737
Kalon Cheung <kalon@berkeley.edu>
2016-04-30 06:50:05 +0000

    Add ability to delete notifications

--- app/controllers/user_activities_controller.rb
+++ app/controllers/user_activities_controller.rb
@@ -4,10 +4,26 @@ class UserActivitiesController < ApplicationController
   def user_activity_params
     params.permit(:user_id, :activity_id, :shift_id)
   end
-
+  
+  def delete
+    id = params[:id]
+    to_delete = UserActivity.find(id)
+    if !to_delete.nil? && @user.id == to_delete.owner_id
+      to_delete.destroy
+    end
+    redirect_to user_activity_path
+    return
+  end
+  
+  def show
+    if @user.nil?
+      redirect_to root_path
+    end
+  end
+    
   def parse_activities
       @activities = UserActivity.where(owner_id: @user).order(created_at: :desc)
   end
- 
+
 
 end
