d88ae95407d59203a7d7b1685cef325c19a3ccbb
Viraj Mahesh <virajmahesh@gmail.com>
2016-05-02 12:18:11 -0700

    Fix missing email config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "icsi-shift-scheduler-preview.herokuapp.com" }
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => "587",
+      :domain               => "gmail.com",
+      :user_name            => "uprise.shift.scheduler@gmail.com",
+      :password             => "helloworld123",
+      :authentication       => :login,
+      :enable_starttls_auto => true
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
