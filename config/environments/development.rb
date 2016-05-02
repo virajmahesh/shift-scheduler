8fe9cb44306d5ccce5955b944222393d5c5cd5f2
Viraj Mahesh <virajmahesh@gmail.com>
2016-05-02 12:18:11 -0700

    Fix missing email config in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "icsi-shift-scheduler-preview.herokuapp.com" }
   config.action_mailer.smtp_settings = {
         :address              => "smtp.gmail.com",
         :port                 => "587",
