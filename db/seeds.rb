8da1f117779179fbe2baf46b083880876fe3d2e5
Viraj Mahesh <virajmahesh@gmail.com>
2016-05-16 02:54:45 +0400

    Read seed data from text files

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,83 +15,14 @@ hillary_shift = Shift.create event: hillary_event, start_time: '10:00 AM', end_t
 
 bernie_shift = Shift.create event: bernie_event, start_time: '11:20 AM', end_time: '10:00 PM',
                              has_limit: 'false',  role: 'Phone Banking'
-                             
-all_skills = Skill.create([{ description: 'Accounting, bookkeeping, compliance' }, 
-                         { description: 'Art and design, by hand' },
-                         { description: 'Art and design, computer aided' },
-                         { description: 'Attending a convention as a delegate' },
-                         { description: 'Being a precinct captain' },
-                         { description: 'Blogging' },
-                         { description: 'Building crowds for events' },
-                         { description: 'Canvassing' },
-                         { description: 'Collecting signatures' },
-                         { description: 'College organizing' },
-                         { description: 'Computer programming' },
-                         { description: 'Cooking/catering' },
-                         { description: 'Data entry' },
-                         { description: 'Data management' },
-                         { description: 'Driving people or making deliveries' },
-                         { description: 'Election day or other GOTV operations' },
-                         { description: 'Issue and policy research' },
-                         { description: 'Legal assistance, voter protection' },
-                         { description: 'Literature drops' },
-                         { description: 'Making fundraising calls' },
-                         { description: 'Media monitoring' },
-                         { description: 'Online research' },
-                         { description: 'Opposition research' },
-                         { description: 'Opposition tracking' },
-                         { description: 'Organizing an event in your home' },
-                         { description: 'Organizing an event outside your home' },
-                         { description: 'Outreach to issue or identity groups' },
-                         { description: 'Outreach to social or professional organizations' },
-                         { description: 'Performance: music/dance/other' },
-                         { description: 'Phone banking' },
-                         { description: 'Press outreach' },
-                         { description: 'Producing a major public event' },
-                         { description: 'Production: photo/video/audio' },
-                         { description: 'Public speaking' },
-                         { description: 'Recruiting volunteers' },
-                         { description: 'Sign assembly and distribution' },
-                         { description: 'Social media' },
-                         { description: 'Stuffing envelopes' },
-                         { description: 'Tabling public events' },
-                         { description: 'Website development' },
-                         { description: 'Writing' },
-                         { description: 'Youth/teen outreach' }])
-                        
-all_issues = Issue.create([ { description: 'Advocacy & Human Rights' },
-                            { description: 'Animal Rights' },
-                            { description: 'Arts & Culture' },
-                            { description: 'Board Development' },
-                            { description: 'Children & Youth' },
-                            { description: 'Community' },
-                            { description: 'Computers & Technology' },
-                            { description: 'Criminal Justice' },
-                            { description: 'Crisis Support' },
-                            { description: 'Disaster Relief' },
-                            { description: 'Economic Justice' },
-                            { description: 'Education & Literacy' },
-                            { description: 'Emergency & Safety' },
-                            { description: 'Employment' },
-                            { description: 'Environment' },
-                            { description: 'Faith-Based' },
-                            { description: 'Health & Medicine' },
-                            { description: 'Homeless & Housing' },
-                            { description: 'Human Trafficking' },
-                            { description: 'Hunger' },
-                            { description: 'Immigrants & Refugees' },
-                            { description: 'International' },
-                            { description: 'Justice & Legal' },
-                            { description: 'LGBT' },
-                            { description: 'Media & Broadcasting' },
-                            { description: 'People With Disabilities' },
-                            { description: 'Politics' },
-                            { description: 'Race & Ethnicity' },
-                            { description: 'Seniors' },
-                            { description: 'Sports & Recreation' },
-                            { description: 'Sustainable Food' },
-                            { description: 'Veterans & Military Families' },
-                            { description: 'Women’s Rights' }])
+
+File.open('db/seed data/skills').each do |skill|
+  Skill.create description: skill
+end
+
+File.open('db/seed data/issues').each do |issue|
+  Issue.create description: issue
+end
 
 UserSkill.create user_id:1, skill_id:3
 UserSkill.create user_id:1, skill_id:5
