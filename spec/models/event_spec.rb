2dd826096338aa4c44ed566e01b76f70bf3ac7d8
Viraj Mahesh <virajmahesh@gmail.com>
2016-05-16 00:41:47 +0400

    Display events in chronological order

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe Event do
 
-  before :each do
-    @user = FactoryGirl.create :user
-    @event = FactoryGirl.create :event, user: @user
-    @issue = FactoryGirl.create :issue
-  end
-
   describe 'has_issue?' do
+    before :each do
+      @user = FactoryGirl.create :user
+      @event = FactoryGirl.create :event, user: @user
+      @issue = FactoryGirl.create :issue
+    end
+
     it 'should return true when the event is linked with the issue' do
       @event.issues << @issue
       @event.save
@@ -16,8 +16,19 @@ describe Event do
       @event.has_issue?(@issue).should == true
     end
 
-    it 'should retrun false when the event is not linked with the issue' do
+    it 'should return false when the event is not linked with the issue' do
       @event.has_issue?(@issue).should == false
+    end
+  end
+
+  describe 'future_events' do
+    it 'should return events sorted in chronological order' do
+      @user = FactoryGirl.create :user
+      @event_1 = FactoryGirl.create :event, user: @user, event_date: 10.days.from_now
+      @event_2 = FactoryGirl.create :event, user: @user, event_date: 5.days.from_now
+
+      Event.future_events.first.should == @event_2
+      Event.future_events.second.should == @event_1
     end
   end
 
