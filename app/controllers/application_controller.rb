b3bbacfe2015f768129910f948728cbe948fd51e
Viraj Mahesh <virajmahesh@gmail.com>
2016-05-17 16:40:29 +0400

    Display a notification badge for unread notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :parse_user  # Parse the user before every controller action
+
+  before_action :parse_user, :count_unread_notifications
+
+  def logged_in
+    not current_user.nil? and not current_user.id.nil?
+  end
+
+  def count_unread_notifications
+    if logged_in
+      @notification_count = UserActivity.where(owner_id: @user.id, read: false).count
+    else
+      @notification_count = 0
+    end
+
+    @unread_notifications = (@notification_count != 0)
+  end
 
   # Parses the current user and stores it in a controller variable
   def parse_user
